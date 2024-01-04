04b2dbd25ab70fc6c1271f97d173c3f487f67fd1
Dragos Catalin <dragoscv12@gmail.com>
2024-01-03 20:52:18 -0800

    Add Next.js configuration and update Tailwind CSS and layout
    This commit adds the necessary configuration for Next.js, including enabling React strict mode and configuring image domains. It also updates the Tailwind CSS configuration to include Next UI and dark mode. Additionally, the layout component is modified to include providers and a header component.

---
+++ app/providers.tsx
@@ -0,0 +1,16 @@
+'use client'
+import { NextUIProvider } from "@nextui-org/react";
+
+export default function Providers({
+    children,
+}: {
+    children: React.ReactNode
+}) {
+    return (
+        <>
+            <NextUIProvider>
+                {children}
+            </NextUIProvider>
+        </>
+    )
+}
