beea94fb80ba9a1edaf3bf48446299131e231477
Dragos Catalin <dragoscv12@gmail.com>
2026-04-07 01:44:34 +0300

    feat(analytics): update Microsoft Clarity tracking ID

--- components/MicrosoftClarity.tsx
+++ components/MicrosoftClarity.tsx
@@ -13,7 +13,7 @@ export function MicrosoftClarity() {
               c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
               t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i+"?ref=bwt";
               y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-          })(window, document, "clarity", "script", "w7o3zp6quu");
+          })(window, document, "clarity", "script", "w7o5mjekah");
         `,
             }}
         />
